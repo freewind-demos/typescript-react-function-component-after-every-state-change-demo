f0aec22950b1606d01d56a034537325dfff96401
Freewind <nowindlee@gmail.com>
2020-02-08 22:58:42 +0800

    typescript-react-function-component-after-every-state-change-demo

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-TypeScript React Hello World Demo
-=================================
+TypeScript React Function Component After Every State Change Demo
+=================================================================
+
+使用`useEffect`，不给第2个参数。
+当state发生变化的时候，它会被调用。
 
 ```
 npm install
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,8 +1,13 @@
-//import * as React from 'react'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 export default function Hello() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log('Dom is updated')
+  });
+
   return <div>
     <h1>Hello React</h1>
+    <button onClick={() => setCount(n => n + 1)}>Click ({count})</button>
   </div>
 };
